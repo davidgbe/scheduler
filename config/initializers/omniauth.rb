347cfb496888bf0db17c7c2760e1b13e675250ce
Michael Chang <mcchang@gmail.com>
2014-08-27 01:12:23 -0700

    redid some stuff on homepage

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-OMNIAUTH_CONFIG = YAML.load_file("#{::Rails.root}/config/omniauth.yml")[::Rails.env]
+# OMNIAUTH_CONFIG = YAML.load_file("#{::Rails.root}/config/omniauth.yml")[::Rails.env]
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, OMNIAUTH_CONFIG['client_id'], OMNIAUTH_CONFIG['secret']
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, OMNIAUTH_CONFIG['client_id'], OMNIAUTH_CONFIG['secret']
+# end
