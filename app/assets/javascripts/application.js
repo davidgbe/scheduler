d148a9643f36829a759b73e731d6672677d80e95
David Bell <davidgbe@usc.edu>
2014-08-30 22:14:44 -0700

    fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,5 +21,4 @@
 //= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
-//= require_tree ../stylesheets
 //= require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( login.css )
+Rails.application.config.assets.precompile += %w( index.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
