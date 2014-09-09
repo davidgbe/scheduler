4898a904069d1fa1faa2e73e56dd0762422004c1
David Bell <davidgbe@usc.edu>
2014-09-09 14:33:38 -0700

    mostly unwritten main and about views

---
+++ app/assets/javascripts/views/about.js
@@ -0,0 +1,10 @@
+Scheduler.Views.About = Backbone.View.extend({
+  template: JST['client/about_template'],
+  events: {},
+  initialize: function() {
+
+  },
+  render: function() {
+    
+  }
+})
---
+++ app/assets/javascripts/views/main.js
@@ -0,0 +1,14 @@
+Scheduler.Views.Main = Backbone.View.extend( {
+  template: "",
+  events: {},
+  initialize: function() {
+
+  },
+  render: function() {
+
+  },
+  createChildViews: function() {
+    
+  }
+
+})
